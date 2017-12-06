9af634c339b597aef316e0dd355b57265e56e472
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-12-06 11:58:31 -0700

    game is largely functional, now to solidify css

--- src/App.js
+++ src/App.js
@@ -5,43 +5,37 @@ import ClickyCard from './components/ClickyCard';
 import clickyTargets from './clickyCards.json';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Modal from "./components/Modal";
 
 class App extends Component {
   state = {
     score: 0,
     highScore: 0,
-    clickyTargets: clickyTargets
+    clickyTargets: clickyTargets,
+    isModalOpen: false,
+    modalTitle: 'Default Title',
+    modalText: 'Default Text'
   }
 
   clickTarget = id => {
     this.state.clickyTargets.forEach((target) => {
       if (target.id === id){
-        // Player lost (clicked same thing again)
+        // Player lost (clicked same thing again IE the target is set to clicked=true previously)
         if (target.clicked) {
           console.log(`GAME OVER!  (previously clicked)`);
-
-          alert (`Game Over!  Score: ${this.state.score} - High Score: ${this.state.highScore}`);
+          // alert (`Game Over!  Score: ${this.state.score} - High Score: ${this.state.highScore}`);
           // TODO show game over modal (high score - etc)
-
-          this.state.clickyTargets.forEach((target) => {
-            // reset clicks so game can start again
-            target.clicked = false;
-          });
-          this.setState({score: 0});
+          this.setState({
+            modalText: `You've lost and clicked the same card twice!  Click outside the box to play again!`,
+            modalTitle: 'Oops, try again!'
+          })
+          this.openModal();
+          this.resetGame();
           return false;
         }
-        // Player did NOT lose - did we win?
-        // TODO this doesn't work as we're getting a dated score - thow this to another layer
-        // else if (this.checkForWin()){
-        //   console.log(`All elements have been clicked - display 'you won!'`);
-        //   // TODO do something for a win and reset the game
-        //   return false;
-        // }
-        // Player did NOT lose and did NOT win - so we continue
+        // Player did NOT lose; win is checked when we update score - so we continue
         else {
           this.updateScore();
-          
-          // continue game and set this target to true
           target.clicked = true;
         }
         if (this.state.score >= this.state.highScore) {
@@ -51,7 +45,7 @@ class App extends Component {
     });
   }
 
-  // Shuffle brought to you by:  https://bost.ocks.org/mike/shuffle/
+  // Shuffle brought to us by:  https://bost.ocks.org/mike/shuffle/
   // (and slighly modified by me) :)
   shuffleCards = (array) => {
     var copy = [], n = array.length, i;
@@ -82,10 +76,35 @@ class App extends Component {
   }
 
   checkForWin = () => {
-    console.log(`checking for win - score: ${this.state.score} out of ${this.state.clickyTargets.length}`)
     if (this.state.score === this.state.clickyTargets.length) {
-      alert(`You've won!!!!`);
+      this.setState({
+        modalText: `You've won!  Click outside the box to play again!`,
+        modalTitle: 'Congratulations!'
+      })
+      this.openModal();
+      this.resetGame();
     }
+    else {
+      setTimeout(() => this.shuffleCards(this.state.clickyTargets),300);
+    }
+  }
+
+  resetGame = () => {
+    console.log(`Game was won or lost - reseting it.`)
+    this.state.clickyTargets.forEach((target) => {
+      // reset clicks so game can start again
+      target.clicked = false;
+    });
+    this.setState({score: 0});
+  }
+
+  // Modal brought to us by:  https://peteris.rocks/blog/modal-window-in-react-from-scratch/
+  openModal() {
+    this.setState({ isModalOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ isModalOpen: false })
   }
 
   render() {
@@ -94,12 +113,17 @@ class App extends Component {
         <Header score={this.state.score} highScore={this.state.highScore} />
         {this.state.clickyTargets.map((target) => {
           return <ClickyCard 
-                  {...target}
-                  key={target.id}
-                  clickTarget={this.clickTarget}
-                  shuffleCards={() => {this.shuffleCards(this.state.clickyTargets)}}
-                  />
+            {...target}
+            key={target.id}
+            clickTarget={this.clickTarget}
+            shuffleCards={() => {this.shuffleCards(this.state.clickyTargets)}}
+          />
         })}
+        <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
+          <h1>{this.state.modalTitle}</h1>
+          <p>{this.state.modalText}</p>
+          <p><button onClick={() => this.closeModal()}>Close</button></p>
+        </Modal>
         <Footer />
       </Wrapper>
     );
--- src/components/ClickyCard/ClickyCard.js
+++ src/components/ClickyCard/ClickyCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./ClickyCard.css";
 
 const ClickyCard = (props) => (
-  // <div className="card" onClick={() => props.shuffleCards()}>
   <div className="card">
     <div className="img-container" dataid={props.id} onClick={() => props.clickTarget(props.id)} >
       <img
